b70a96362d1847ab0f79701d6251159f35b695ab
David Zhao <davidzhao058@gmail.com>
2017-08-08 22:04:42 -0400

    Plot final buckets

--- k-means.py
+++ k-means.py
@@ -20,24 +20,30 @@ class Clusterer:
         #                 20% to cross-validation
         #                 20% to test
         _train, _cv, _test = np.split(points, [int(points.shape[0] * 0.6), int(points.shape[0] * 0.8)])
-
+        
         # for 25 times:
         #   randomly initialize centroids
         centroids = _train[:nCentroids]
-        buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
-        print(cost)
+        prev_cost = -1
         #   assign points to centroid
         #   calculate cost and gradient
+        buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
+        print(cost)
         #   while gradient > 0.001
+        while prev_cost != cost:
+            prev_cost = cost
         #     assign points to centroid
         #     assign centroid to mean of its assigned points
         #     calculate cost and gradient
+            buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
+            print(cost)
         #   calculate cost on cross-validation set
         # return set of centroids with lowest cost
+        return centroids
         
     def bucketAndUpdate(self, _train, centroids):
         cost = 0
-        buckets = [[]] * centroids.shape[0]
+        buckets = [[] for n in range(centroids.shape[0])]
         for n in range(_train.shape[0]):
             costs = np.sum((_train[n] - centroids) ** 2, axis=1)
             buckets[np.argmin(costs)].append(_train[n])
@@ -48,6 +54,10 @@ class Clusterer:
 
 
 points = generateRandomPoints()
-Clusterer().cluster(points)
-plt.plot(points[:,0], points[:,1], 'bo')
+clusterer = Clusterer()
+centroids = clusterer.cluster(points)
+buckets, tmp, points = clusterer.bucketAndUpdate(points, centroids)
+for n in range(len(buckets)):
+    bucket_points = np.vstack(buckets[n])
+    plt.plot(bucket_points[:,0], bucket_points[:,1], 'o')
 plt.show()
