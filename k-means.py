2a83fc09d990a4031c77f76f288c84373bf6ec5e
David Zhao <davidzhao058@gmail.com>
2017-08-08 22:14:16 -0400

    Add multiple random initializations with test on cross validation set

--- k-means.py
+++ k-means.py
@@ -14,32 +14,29 @@ def generateRandomPoints(nClusters=5, nPerCluster=100):
 
 class Clusterer:
     def cluster(self, points, nCentroids=10):
-        # shuffle data
         np.random.shuffle(points)
         # allocate data - 60% to training
         #                 20% to cross-validation
         #                 20% to test
         _train, _cv, _test = np.split(points, [int(points.shape[0] * 0.6), int(points.shape[0] * 0.8)])
-        
-        # for 25 times:
-        #   randomly initialize centroids
-        centroids = _train[:nCentroids]
-        prev_cost = -1
-        #   assign points to centroid
-        #   calculate cost and gradient
-        buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
-        print(cost)
-        #   while gradient > 0.001
-        while prev_cost != cost:
-            prev_cost = cost
-        #     assign points to centroid
-        #     assign centroid to mean of its assigned points
-        #     calculate cost and gradient
+        best_cost = float('inf')
+        best_centroids = _train[:nCentroids]
+        for n in range(25):
+            np.random.shuffle(_train)
+            centroids = _train[:nCentroids]
+            prev_cost = -1
             buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
             print(cost)
-        #   calculate cost on cross-validation set
-        # return set of centroids with lowest cost
-        return centroids
+            while prev_cost != cost:
+                prev_cost = cost
+                buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
+                # print(cost)
+            cv_cost = self.cost(_cv, centroids)
+            if cv_cost < best_cost:
+                best_cost = cv_cost
+                best_centroids = centroids
+        test_cost = self.cost(_test, best_centroids)
+        return best_centroids, test_cost
         
     def bucketAndUpdate(self, _train, centroids):
         cost = 0
@@ -52,10 +49,18 @@ class Clusterer:
             centroids[n] = np.average(np.vstack(buckets[n]), axis=0)
         return buckets, centroids, cost
 
+    def cost(self, points, centroids):
+        cost = 0
+        for n in range(points.shape[0]):
+            costs = np.sum((points[n] - centroids) ** 2, axis=1)
+            cost += np.min(costs)
+        return cost
+
 
 points = generateRandomPoints()
 clusterer = Clusterer()
-centroids = clusterer.cluster(points)
+centroids, test_cost = clusterer.cluster(points)
+print(test_cost)
 buckets, tmp, points = clusterer.bucketAndUpdate(points, centroids)
 for n in range(len(buckets)):
     bucket_points = np.vstack(buckets[n])
