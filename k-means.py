5a1ccbc1c135aa96395734fa6c92d2a53f467a15
David Zhao <davidzhao058@gmail.com>
2017-08-08 21:02:07 -0400

    Write function to bucket points and reassign centroids

--- k-means.py
+++ k-means.py
@@ -23,6 +23,10 @@ class Clusterer:
 
         # for 25 times:
         #   randomly initialize centroids
+        centroids = _train[:nCentroids]
+        buckets, centroids, cost = self.bucketAndUpdate(_train, centroids)
+        print(cost)
+        #   assign points to centroid
         #   calculate cost and gradient
         #   while gradient > 0.001
         #     assign points to centroid
@@ -30,6 +34,18 @@ class Clusterer:
         #     calculate cost and gradient
         #   calculate cost on cross-validation set
         # return set of centroids with lowest cost
+        
+    def bucketAndUpdate(self, _train, centroids):
+        cost = 0
+        buckets = [[]] * centroids.shape[0]
+        for n in range(_train.shape[0]):
+            costs = np.sum((_train[n] - centroids) ** 2, axis=1)
+            buckets[np.argmin(costs)].append(_train[n])
+            cost += np.min(costs)
+        for n in range(len(buckets)):
+            centroids[n] = np.average(np.vstack(buckets[n]), axis=0)
+        return buckets, centroids, cost
+
 
 points = generateRandomPoints()
 Clusterer().cluster(points)
